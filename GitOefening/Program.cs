711bebe1f9fde3165c502d44b30695050223a43c
Shintarouz <thomas.schaer1@gmail.com>
2026-01-06 11:41:16 +0100

    Eerste Commit
    Test voor finn

--- GitOefening/Program.cs
+++ GitOefening/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Dit is een oefening voor Finn.");
         }
     }
 }
