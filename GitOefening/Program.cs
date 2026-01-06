4e2dd741989596143700ff24168ff7c66a649645
Shintarouz <thomas.schaer1@gmail.com>
2026-01-06 11:54:43 +0100

    Test

--- GitOefening/Program.cs
+++ GitOefening/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Waarom lees je dit?");
+            Console.WriteLine("Dit is een nieuwe regel toegevoegd in VSCode.");
         }
     }
 }
