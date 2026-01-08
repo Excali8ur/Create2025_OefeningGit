a77b6cbdb9818447094df487ffbc0e0601600005
Caska07 <casdautzenberg@gmail.com>
2026-01-08 10:49:10 +0100

    naakt

---
+++ GitOefening/Model/Naaktslak.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverervingDieren.Model
+{
+    internal class Naaktslak
+    {
+		public Naaktslak(string naam, string geluid, int aantalPoten, bool heeftVacht, bool draagtHoed)
+			: base(naam, geluid, aantalPoten, heeftVacht);
+	}
+}
