cc172eaa9ee4fc986cef43bdeea9491a5460eb0f
Blobisblij <samthomsen@ziggo.nl>
2026-01-08 10:49:48 +0100

    beer is af nu

--- GitOefening/Model/Beer.cs
+++ GitOefening/Model/Beer.cs
@@ -1,7 +1,26 @@
 namespace OverervingDieren.Model;
 
-public class Beer
+internal class Beer : Dier
 {
-    private string naam;
+    protected bool houVanZalm;
+    private string ReactieOpZalm;
     
+    public Beer(string naam, string geluid, int aantalPoten, bool heeftVacht, bool houVanZalm) 
+        : base(naam, geluid, aantalPoten, heeftVacht) //Constructor van de basisklasse
+    {
+        this.houVanZalm = houVanZalm;
+    }
+
+    public string houIkVanZalm()
+    {
+        if(houVanZalm == true)
+        {
+            ReactieOpZalm = "Heel erg";
+        }
+        else
+        {
+            ReactieOpZalm = "Helemaal NIET";
+        }
+        return $"{naam} houd  {ReactieOpZalm} van zalm";
+    }
 }
