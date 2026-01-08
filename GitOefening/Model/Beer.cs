4fe10ac2669d123615962e76dccde47dd5f5943c
GinoWalls <ginowalls2006@gmail.com>
2026-01-08 11:14:45 +0100

    Beek & Slak toegevoegd

---
+++ GitOefening/Model/Beer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverervingDieren.Model
+{
+    internal class Beer : Dier
+    {
+        private bool boos;
+
+        public Beer(string naam, string geluid, int aantalPoten, bool heeftVacht)
+            : base(naam, geluid, aantalPoten, heeftVacht)
+        {
+            this.boos = boos;
+        }
+        public void boosJa()
+        {
+            boos = true;
+        }
+        public void boosNee()
+        {
+            boos = false;
+        }
+    }
+}
---
+++ GitOefening/Model/Slak.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverervingDieren.Model
+{
+    internal class Slak : Dier
+    {
+        private bool huis;
+
+        public Slak(string naam, string geluid, int aantalPoten, bool heeftVacht)
+            : base(naam, geluid, aantalPoten, heeftVacht)
+        {
+            this.huis = huis;
+        }
+        public void HuisHeb()
+        {
+            huis = true;
+        }
+        public void HuisWeg()
+        {
+            huis = false;
+        }
+    }
+}
