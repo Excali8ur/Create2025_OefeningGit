373fc6c03f204e987299e935a6cde23aaebf8b2f
BramNoortman <91888756+BramNoortman@users.noreply.github.com>
2026-01-08 11:06:27 +0100

    Hert aangemaakt en toegevoegd aan program

---
+++ GitOefening/Model/Hert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverervingDieren.Model
+{
+    internal class Hert : Dier
+    {
+        //Eigenschappen specifiek voor Volgelbekdieren
+        private bool zietkoplampen;
+
+        //constructor
+        public Hert(string naam, string geluid, int aantalPoten, bool heeftVacht, bool zietkoplampen)
+    : base(naam, geluid, aantalPoten, heeftVacht) //Constructor van de basisklasse
+        {
+            this.zietkoplampen = zietkoplampen;
+        }
+
+        public void WordtAangereden()
+        {
+            zietkoplampen = true;
+        }
+
+        public void LooptVerder()
+        {
+            zietkoplampen = false;
+        }
+    }
+}
--- GitOefening/Program.cs
+++ GitOefening/Program.cs
@@ -12,6 +12,7 @@ namespace OverervingDieren
             Dier perry = new Dier("Perry", "Prrrr", 4, true);
             Dier barry = new Dier("Barry", "Growl", 4, true);
             Dier gerry = new Dier("Gerry", "Sjlur", 0, false);
+            
 
             Console.WriteLine( perry.MaakGeluid());
             Console.WriteLine( gerry.MaakGeluid());
@@ -20,9 +21,12 @@ namespace OverervingDieren
             Console.WriteLine(geluid);
 
             Vogelbekdier vogelbekdier = new Vogelbekdier("Perry de tweede", "Prrrr", 4, true, false);
+            Hert rudolph = new Hert("Larry", "autongelukgeluiden", 4, true, true);
 
             Console.WriteLine(vogelbekdier.MaakGeluid());
+            Console.WriteLine(rudolph.MaakGeluid());
             vogelbekdier.ZetHoedOp();
+            rudolph.WordtAangereden();
         }
     }
 }
