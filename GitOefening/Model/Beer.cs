8258e6d140b00832284e5d86cc537e27afef8ab2
Vonckje <2503129voncken@zuyd.nl>
2026-01-08 11:11:49 +0100

    beer doet grom

---
+++ GitOefening/Model/Beer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverervingDieren.Model
+{
+    internal class Beer : Dier
+    {
+        //Constructor
+        public Beer(string naam, string geluid, int aantalPoten, bool heeftVacht)
+            : base(naam, geluid, aantalPoten, heeftVacht)
+        {
+            this.geluid = "Grom";
+        }
+
+    }
+}
+
