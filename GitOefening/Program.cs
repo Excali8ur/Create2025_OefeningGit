c4acc84cd139fef031c26fb9a950ec57c7636a98
Ayman Hasnaoui <2405182elhasnaoui@zuyd.nl>
2026-01-08 11:12:50 +0100

    Aap klasse toegevoegd

--- GitOefening/Program.cs
+++ GitOefening/Program.cs
@@ -8,21 +8,25 @@ namespace OverervingDieren
         {
             Console.WriteLine("Hello, World!");
 
+            Dier perry = new("Perry", "Prrrr", 4, true);
+            Dier barry = new("Barry", "Growl", 4, true);
+            Dier gerry = new("Gerry", "Sjlur", 0, false);
+            Dier pipi = new("pipi", "oe-oe-oe", 2, true);
 
-            Dier perry = new Dier("Perry", "Prrrr", 4, true);
-            Dier barry = new Dier("Barry", "Growl", 4, true);
-            Dier gerry = new Dier("Gerry", "Sjlur", 0, false);
-
-            Console.WriteLine( perry.MaakGeluid());
-            Console.WriteLine( gerry.MaakGeluid());
+            Console.WriteLine(perry.MaakGeluid());
+            Console.WriteLine(gerry.MaakGeluid());
 
             string geluid = barry.MaakGeluid();
             Console.WriteLine(geluid);
 
-            Vogelbekdier vogelbekdier = new Vogelbekdier("Perry de tweede", "Prrrr", 4, true, false);
+                Vogelbekdier vogelbekdier = new("Perry de tweede", "Prrrr", 4, true, false);
+                Aap aap = new("pipi", "oe - oe - oe", 2, true, false);
 
             Console.WriteLine(vogelbekdier.MaakGeluid());
             vogelbekdier.ZetHoedOp();
+            
+            Console.WriteLine(aap.MaakGeluid());
+            aap.TrektKlerenAan();
         }
     }
 }
