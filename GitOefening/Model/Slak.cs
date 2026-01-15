71d4126d2c9e5672e274460d7afc58ca500ba31b
greenlopit <2502547schoenmakers@zuyd.nl>
2026-01-15 14:05:17 +0100

    slak

---
+++ GitOefening/Model/Slak.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverervingDieren.Model
+{
+    internal class Slak : Dier
+    {
+        //Specifieke eigenschap voor alleen Slak
+        private bool heeftHuisje;
+
+        //Consctructor
+        public Slak(string naam, string geluid, int aantalPoten, bool heeftVacht, bool heeftHuisje)
+           : base(naam, geluid, aantalPoten, heeftVacht)
+        {
+            this.heeftHuisje = heeftHuisje;
+        }
+
+        public void HeeftHuisjeWel()
+        {
+            heeftHuisje = true;
+        }
+
+        public void HeeftHuisjeNiet()
+        {
+            heeftHuisje = false;
+        }
+    }
+
+}
